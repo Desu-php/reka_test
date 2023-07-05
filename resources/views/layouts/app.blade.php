2aae7a0d586dd13df9b0996b9990286057140e53
Bakhtiyor <teaddyson@gmail.com>
2023-07-05 21:48:04 +0500

    js moved

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+    @vite(['resources/sass/app.scss'])
 </head>
 <body>
 <div id="app">
@@ -86,6 +86,7 @@
         @yield('content')
     </main>
 </div>
+@vite('resources/js/app.js')
 @stack('scripts')
 </body>
 </html>
